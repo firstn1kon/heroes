d40062ae15c4a600a8cdb7c0548dddc6695deb6b
Павел Семенов <pavelsemenov@MacBook-Air-Pavel.local>
2022-10-10 18:08:55 +0300

    filter in useselector without switch case

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -1,21 +1,20 @@
 import {useHttp} from '../../hooks/http.hook';
-import { useEffect, useCallback } from 'react';
+import { useEffect, useCallback} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { heroesFetching, heroesFetched, heroesFetchingError, deleteHeroe } from '../../actions';
 import HeroesListItem from "../heroesListItem/HeroesListItem";
 import Spinner from '../spinner/Spinner';
 
-// Задача для этого компонента:
-// При клике на "крестик" идет удаление персонажа из общего состояния
-// Усложненная задача:
-// Удаление идет и с json файла при помощи метода DELETE
-
 const HeroesList = () => {
-    const {heroes, heroesLoadingStatus, selectedFilter} = useSelector(state => state);
+    const {heroesLoadingStatus} = useSelector(state => state);
+    const heroes = useSelector(state => 
+        state.selectedFilter === "all" ? 
+            state.heroes : 
+            state.heroes.filter(item => item.element === state.selectedFilter) 
+    );
     const dispatch = useDispatch();
     const {request} = useHttp();
-
     useEffect(() => {
         dispatch(heroesFetching());
         request("http://localhost:3001/heroes")
@@ -34,12 +33,6 @@ const HeroesList = () => {
         
     },[dispatch, request]);
 
-    if (heroesLoadingStatus === "loading") {
-        return <Spinner/>;
-    } else if (heroesLoadingStatus === "error") {
-        return <h5 className="text-center mt-5">Ошибка загрузки</h5>
-    }
-
     const renderHeroesList = (arr) => {
         console.log('render')
         if (arr.length === 0) {
@@ -51,25 +44,15 @@ const HeroesList = () => {
         })
     }
 
-    const elements = renderHeroesList(filter(heroes, selectedFilter));
+    if (heroesLoadingStatus === "loading") {
+        return <Spinner/>;
+    } else if (heroesLoadingStatus === "error") {
+        return <h5 className="text-center mt-5">Ошибка загрузки</h5>
+    }
     
 
-    function filter (data, filter) {
-        switch(filter) {
-            default: 
-                return data;
-            case "all": 
-                return data;
-            case "fire": 
-                return data.filter(item => item.element === "fire");
-            case "water": 
-                return data.filter(item => item.element === "water");
-            case "wind": 
-                return data.filter(item => item.element === "wind");
-            case "earth": 
-                return data.filter(item => item.element === "earth");
-        }
-    }
+    const elements = renderHeroesList(heroes);
+
     return (
         <ul>
             {elements}
