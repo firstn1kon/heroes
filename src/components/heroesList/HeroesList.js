aa50925a46234b37dab3b92ccca3ce1cfcabb01b
Павел Семенов <pavelsemenov@MacBook-Air-Pavel.local>
2022-10-10 18:49:04 +0300

    createSelecor for optimization memo

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -1,18 +1,23 @@
 import {useHttp} from '../../hooks/http.hook';
-import { useEffect, useCallback} from 'react';
+import { useEffect, useCallback, useMemo} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { createSelector } from 'reselect';
 
 import { heroesFetching, heroesFetched, heroesFetchingError, deleteHeroe } from '../../actions';
 import HeroesListItem from "../heroesListItem/HeroesListItem";
 import Spinner from '../spinner/Spinner';
 
 const HeroesList = () => {
-    const {heroesLoadingStatus} = useSelector(state => state);
-    const heroes = useSelector(state => 
-        state.selectedFilter === "all" ? 
-            state.heroes : 
-            state.heroes.filter(item => item.element === state.selectedFilter) 
-    );
+    const heroesSelector = createSelector(
+        (state) => state.selectedFilter,
+        (state) => state.heroes,
+        (filter, heroes) => filter === "all" ? 
+        heroes : 
+        heroes.filter(item => item.element === filter) 
+
+    )
+    const heroesLoadingStatus = useSelector(state => state.heroesLoadingStatus);
+    const heroes = useSelector(heroesSelector);
     const dispatch = useDispatch();
     const {request} = useHttp();
     useEffect(() => {
@@ -31,7 +36,7 @@ const HeroesList = () => {
             .then(()=> dispatch(deleteHeroe(id)))
             .catch(() => dispatch(heroesFetchingError()))
         
-    },[dispatch, request]);
+    },[request]);
 
     const renderHeroesList = (arr) => {
         console.log('render')
@@ -44,14 +49,14 @@ const HeroesList = () => {
         })
     }
 
+
+    const elements = renderHeroesList(heroes);
+
     if (heroesLoadingStatus === "loading") {
         return <Spinner/>;
     } else if (heroesLoadingStatus === "error") {
         return <h5 className="text-center mt-5">Ошибка загрузки</h5>
     }
-    
-
-    const elements = renderHeroesList(heroes);
 
     return (
         <ul>
